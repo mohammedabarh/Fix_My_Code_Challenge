3f51c393138c464a8b040b24873077479fc647b6
mohammedabarh <abmohammed606@gmail.com>
2024-12-01 13:44:11 -0800

    Fix_My_Code_Challenge

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,10 +14,15 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+let size = parseInt(process.argv[2], 10);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+if (isNaN(size) || size <= 0) {
+    process.stderr.write("Size must be a positive number\n");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
